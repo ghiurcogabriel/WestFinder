7b79723b1e776960e7f8002e9da38f2691e93b23
Ghiurco Gabriel <ghiurcogabriel@gmail.com>
2023-05-10 18:46:12 +0300

    deploy

--- src/ContextApi/cart/CartReducer.js
+++ src/ContextApi/cart/CartReducer.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART, REMOVE_FROM_CART, SHOW_HIDE_CART, INCREASE_CART, DECREASE_CART } from "../Types";
+import { ADD_TO_CART, REMOVE_FROM_CART, SHOW_HIDE_CART, INCREASE_CART } from "../Types";
 
 const CartReducer = (state, action) => {
   switch (action.type) {
